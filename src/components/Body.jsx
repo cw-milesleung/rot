b8a058b573e9b6256cf2d301056359937f4cae07
miles25 <miles@databased.com>
2023-12-18 23:16:52 -0700

    first commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,17 @@
+const Body = () => {
+  return (
+    <div>
+      <label>Input: </label>
+      <p>
+        <textarea type="text" />
+      </p>
+      <p>↓</p>
+      <p>↓</p>
+      <label>Output: </label>
+      <p>
+        <textarea type="text" readOnly />
+      </p>
+    </div>
+  );
+};
+export default Body;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <>
+      <h1>ROT13</h1>
+      <a href="https://en.wikipedia.org/wiki/ROT13">About ROT13</a>
+    </>
+  );
+};
+export default Header;
