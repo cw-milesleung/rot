fc754f036b2c2fbb74f4f5f3b5947668478e5364
miles25 <miles@databased.com>
2023-12-19 16:51:30 -0700

    first commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,40 @@
+import { useRef, useState } from "react";
+import { rotArrayObj } from "../data.js";
+
 const Body = () => {
+  const [rot, setRot] = useState(13);
+  const inputText = useRef();
+
+  const handleRotSelection = (e) => {
+    e.target.value;
+    console.log(e.target.value);
+  };
+
   return (
     <div>
-      <label>Input: </label>
+      <label>Input Text: </label>
       <p>
         <textarea type="text" />
       </p>
       <p className="arrow">↓</p>
+      <p>
+        <select
+          onChange={handleRotSelection}
+          // defaultValue={rotArrayObj[12].rot}
+        >
+          {rotArrayObj.map((item) => (
+            <option
+              key={item.value}
+              value={item.value}
+              selected={item.rot === "ROT13"}
+            >
+              {item.rot}
+            </option>
+          ))}
+        </select>
+      </p>
       <p className="arrow">↓</p>
-      <label>Output: </label>
+      <label>Result: </label>
       <p>
         <textarea type="text" readOnly />
       </p>
